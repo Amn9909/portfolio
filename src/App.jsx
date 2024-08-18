18f5dfed90824e51e195fadb34a8f19290f73598
aman <118736234+amnak09@users.noreply.github.com>
2024-08-18 21:31:31 +0530

    added chnages

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react'
+import Home from './pages/Home'
+import { Box } from '@mui/material'
+
+const App = () => {
+  return (
+   <Box sx={{color : 'white'}}>
+   <Home />
+   </ Box>
+  )
+}
+
+export default App
